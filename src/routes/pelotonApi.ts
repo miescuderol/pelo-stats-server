fa3e035aa305b2fac29dbcf43597b79245ca6b40
Nacha Escudero Leoz <miescuderol@gmail.com>
2021-10-11 18:45:49 -0700

    workouts enpoint working. session id passed as authentication header. Needs https

--- src/routes/pelotonApi.ts
+++ src/routes/pelotonApi.ts
@@ -4,11 +4,28 @@ import express from 'express';
 export const router = express.Router();
 
 const PELOTON_BASE_URL = 'https://api.onepeloton.com/';
+const USER_API = 'api/user/';
 
-// router.use(cors());
 router.post('/auth', (req, res) => {
-    axios.post(`${PELOTON_BASE_URL}auth/login`, req.body).then(response => res.send(response.data))
-
+    axios.post(`${PELOTON_BASE_URL}auth/login`, req.body)
+    .then(response => 
+        res.send(response.data)
+    )
 })
 
-
+router.get('/user/:userId/workouts', (request, response) => {
+    axios.get(`${PELOTON_BASE_URL}${USER_API}${request.params.userId}/workouts`, {
+        headers: {
+            accept: 'application/json',
+            cookie: request.header('authorization'),
+            'peloton-platform': 'web',
+        },
+        params: {
+            joins: 'ride',
+            limit: 10,
+            page: 0
+        }
+    }).then((pelotonResponse => {
+        response.send(pelotonResponse.data)
+    }))
+})
